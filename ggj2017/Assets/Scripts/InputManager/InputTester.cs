0df74555f79e6a8d6585ae2c4ac48379c137a2b7
Stijn Delaruelle <stijn.delaruelle@gmail.com>
2017-01-20 12:59:57 +0100

    Initial commit.

---
+++ ggj2017/Assets/Scripts/InputManager/InputTester.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sjabloon;
+
+public class InputTester : MonoBehaviour
+{
+    private void Start()
+    {
+        InputManager.Instance.BindButton("Test", KeyCode.A, InputManager.ButtonState.OnRelease);
+        InputManager.Instance.BindButton("Test_2", 0, ControllerButtonCode.A, InputManager.ButtonState.OnRelease);
+    }
+
+    private void Update()
+    {
+        bool keyboardPressed = InputManager.Instance.GetButton("Test");
+        bool controllerPressed = InputManager.Instance.GetButton("Test_2");
+
+        if (keyboardPressed)
+        {
+            Debug.Log("Keyboard pressed");
+        }
+
+        if (controllerPressed)
+        {
+            Debug.Log("Controller pressed");
+        }
+    }
+}
