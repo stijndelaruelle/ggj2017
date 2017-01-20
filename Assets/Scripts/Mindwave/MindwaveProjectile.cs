1e48a3c7697e439d633ae22a5a320123eb3e3567
DESKTOP-EAGK21I\Michel <fusedotcore@gmail.com>
2017-01-21 00:58:15 +0100

    brainwave trail

---
+++ Assets/Scripts/Mindwave/MindwaveProjectile.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MindwaveProjectile : MonoBehaviour {
+
+    bool Move = false;
+    Vector3 StartPos, TargetPos;
+    float TravelTime;
+    float TravelTimer;
+    float Phase2height;
+    float Frequency, Amplitude;
+    bool DistanceBasedFrequency;
+    static Transform TargetTransform;
+    bool FollowObject;
+    
+
+    public void Initialize(Transform targetTransform, float speed, float phase2height, float freq, float amp, bool distancebasedfreq, bool followObj) {
+        //receive the information needed to create the path
+        TargetPos = targetTransform.position;
+        FollowObject = followObj;
+        if(followObj) TargetTransform = targetTransform;
+        StartPos = transform.position;
+        Phase2height = phase2height;
+        Amplitude = amp;
+        Frequency = freq;
+        DistanceBasedFrequency = distancebasedfreq;
+        
+        //calculate time it should take
+        TravelTime = Vector3.Distance(TargetPos, transform.position)/speed;
+
+        
+        if(DistanceBasedFrequency) {
+            Frequency = freq * TravelTime;
+        }
+
+        Move = true;
+    }
+
+    void Update() {
+        if(!Move) return;
+
+        //    __^_ _^_   
+        //   / .  v . \   < hoot
+        //  / /\/\/\/\ \ 
+
+        //take care of horizontal movement
+        TravelTimer += Time.deltaTime;
+        float travelPercentage = TravelTimer/TravelTime;
+        if(FollowObject) TargetPos = TargetTransform.transform.position;
+        Vector3 pos = (TargetPos - StartPos) * travelPercentage;
+        //float distance = Vector3.Distance(pos, transform.position);
+
+
+        if(travelPercentage<0.15f) {
+            //phase one
+            //upwards move + move towards target
+            //15%
+            pos.y +=  Phase2height * (travelPercentage/0.15f);
+
+        }else if(travelPercentage>=0.15f && travelPercentage<0.85f) {
+            //phase two
+            //sine waving at height
+            //60%
+
+            //calculate a sine wave based on the provided frequency and amplitude
+            pos.y = Mathf.Sin(Frequency*travelPercentage) * Amplitude + Phase2height;
+
+        }else {
+            //phase three
+            //move downwards + toward target
+            //15%
+            pos.y +=  Phase2height-Phase2height * ((travelPercentage-0.85f)/0.15f); 
+
+        }
+
+        transform.position = pos;
+        
+        if(travelPercentage>1) {
+            Move = false;
+            Destroy(gameObject, GetComponent<TrailRenderer>().time);
+        }
+        
+    }
+}
---
+++ Assets/Scripts/Mindwave/MindwaveSelector.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MindwaveSelector : MonoBehaviour {
+
+    //draws a line between this object and the target.
+    
+    [SerializeField] GameObject Target;
+    [SerializeField] GameObject ProjectilePrefab;
+    [SerializeField] float Speed = 1;
+    [SerializeField] float EmitInterval = 1;
+    [SerializeField] float ProjectilePhase2Height = 5;
+    [SerializeField] float Frequency = 1;
+    [SerializeField] float Amplitude = 1;
+    [SerializeField] bool DistanceBasedFrequency = true;
+    [SerializeField] bool FollowObj = true;
+
+
+    float EmitIntervalTimer = 0;
+
+    public void SetTarget(Transform target) {
+        Target.transform.position = target.position;
+    }
+
+    void Update() {
+        EmitIntervalTimer+= Time.deltaTime;
+        if(EmitIntervalTimer>EmitInterval) {
+            EmitIntervalTimer = 0;
+            Emit();
+        }
+    }
+
+    void Emit() {
+        //shoot off a trailrenderer object to the target.
+        GameObject projectile = Instantiate(ProjectilePrefab, transform);
+        projectile.transform.position = transform.position;
+
+        projectile.GetComponent<MindwaveProjectile>().Initialize(Target.transform, Speed, ProjectilePhase2Height, Frequency, Amplitude, DistanceBasedFrequency, FollowObj);
+    }
+
+}
