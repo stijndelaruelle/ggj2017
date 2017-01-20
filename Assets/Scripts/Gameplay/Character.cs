de5e9fcf73bc44f3998c23eff6c90f92070be7be
Stijn Delaruelle <stijn.delaruelle@gmail.com>
2017-01-20 20:43:11 +0100

    Fixed nested folder

---
+++ Assets/Scripts/Gameplay/Character.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    [SerializeField]
+    private float m_Frequency;
+
+    [SerializeField]
+    private string m_HashTag;
+
+    [SerializeField]
+    private float m_Width;
+    public float Width
+    {
+        get { return m_Width; }
+    }
+
+    public void SetPosition(Vector3 position)
+    {
+        transform.position = position;
+    }
+}
---
+++ Assets/Scripts/Gameplay/Queue.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Queue : MonoBehaviour
+{
+    [SerializeField]
+    private Transform m_StartPosition;
+
+    [SerializeField]
+    private List<Character> m_Characters;
+
+    private void Start()
+    {
+        //Set all the characters in the right place
+        Vector3 nextPosition = m_StartPosition.position;
+
+        foreach (Character character in m_Characters)
+        {
+            character.SetPosition(nextPosition);
+            nextPosition.x -= character.Width;
+        }
+    }
+}
