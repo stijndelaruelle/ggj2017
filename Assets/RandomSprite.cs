123f127ee00915a567a2aca70025e9844531e39b
DESKTOP-EAGK21I\Michel <fusedotcore@gmail.com>
2017-01-21 18:57:40 +0100

    Fixed wrong gameover in queue

---
+++ Assets/RandomSprite.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSprite : MonoBehaviour {
+
+	
+    [SerializeField] List<Sprite> SpriteList = new List<Sprite>();
+
+    void Start() {
+        GetComponent<SpriteRenderer>().sprite = SpriteList[Random.Range(0,SpriteList.Count-1)];
+    }
+}
