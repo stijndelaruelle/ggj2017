a08771d0573191595d929bb14afc44e31799f76c
DESKTOP-EAGK21I\Michel <fusedotcore@gmail.com>
2017-01-21 17:00:39 +0100

    credit in splash

---
+++ Assets/MoveCameraTo.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCameraTo : MonoBehaviour {
+
+	[SerializeField] GameObject target;
+
+    void OnMouseUp() {
+        iTween.MoveTo(Camera.main.gameObject, iTween.Hash("y", target.transform.position.y, "easetype", "easeInOutQuad", "time", 0.5f));
+    }
+}
