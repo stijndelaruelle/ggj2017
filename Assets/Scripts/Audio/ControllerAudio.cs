0ce4210d3be20f8e60ad388046591be995a67000
Dave Van Verdegem <dave@pajamallama.be>
2017-01-21 22:00:21 +0100

    added audioscript

---
+++ Assets/Scripts/Audio/ControllerAudio.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ControllerAudio : MonoBehaviour
+{
+	#region Fields
+	private audio_pitch _audioPitch;
+	#endregion
+
+
+	#region Life Cycle
+	// Use this for initialization
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+	#endregion
+
+	#region Methods
+	
+	#endregion
+}
