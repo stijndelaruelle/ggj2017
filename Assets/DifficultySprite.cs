2455a4a41a0fd71fbae4076ae38977496cf50245
Stijn Delaruelle <stijn.delaruelle@gmail.com>
2017-01-22 12:51:00 +0100

    Fixed the in-game UI

---
+++ Assets/DifficultySprite.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DifficultySprite : MonoBehaviour
+{
+    [SerializeField]
+    List<Sprite> SpriteList = new List<Sprite>();
+
+    void Start()
+    {
+        GetComponent<Image>().sprite = SpriteList[PlayerPrefs.GetInt("DifficultyMode")];
+    }
+}
