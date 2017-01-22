e9fd1ba5516aa6f79b536d9075455bddfb25a6a9
DESKTOP-EAGK21I\Michel <fusedotcore@gmail.com>
2017-01-22 02:21:31 +0100

    preparations for flicker

--- Assets/FlickerSprite.cs
+++ Assets/FlickerSprite.cs
@@ -8,22 +8,39 @@ public class FlickerSprite : MonoBehaviour {
     [SerializeField] float FlickerIntervalMin=0, FlickerIntervalMax=1;
     [SerializeField] float FlickerTime=0.1f;
 
+    [SerializeField] List<FlickerSprite> LinkedFlickers = new List<FlickerSprite>();
+
+    bool LinkedSprite = false;
     float flickerTimer=0;
     SpriteRenderer spRen;
     
     void Start() {
         flickerTimer = Random.Range(FlickerIntervalMin, FlickerIntervalMax);
         spRen = GetComponent<SpriteRenderer>();
+        foreach(FlickerSprite flsp in LinkedFlickers) {
+            flsp.Link(FlickerTime);
+        }
+    }
+
+    void Link(float flickerTime) {
+        FlickerTime  = flickerTime;
+        LinkedSprite = true;
     }
 
     void Update() {
         flickerTimer-=Time.deltaTime;
 
         if(flickerTimer<=0) {
-            flickerTimer = Random.Range(FlickerIntervalMin, FlickerIntervalMax);
-            spRen.sprite = SpriteB;
-            Invoke("RestoreInitialSprite", FlickerTime);
+            foreach(FlickerSprite flsp in LinkedFlickers) {
+                flsp.Flicker();
+            }
         }
+    }
+
+    void Flicker() {
+        flickerTimer = Random.Range(FlickerIntervalMin, FlickerIntervalMax);
+        spRen.sprite = SpriteB;
+        Invoke("RestoreInitialSprite", FlickerTime);
     }
 
     void RestoreInitialSprite() {
